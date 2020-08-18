73fd49e26fc522a55f96003857a1548614ddd9f6
jusira <alicjagosiewska@gmail.com>
2020-08-18 15:49:24 +0200

    small fix

--- benchmark/generate_results.R
+++ benchmark/generate_results.R
@@ -4,8 +4,8 @@ library(dplyr)
 load("./benchmark/packages_times.rda")
 
 packages_times %>%
-  select(pkg_name, type, evaluation_time) %>%
-  group_by(pkg_name, type) %>%
+  select(pkg_name, evaluation_time) %>%
+  group_by(pkg_name) %>%
   mutate(min = min(evaluation_time),
          mean = mean(evaluation_time),
          max = max(evaluation_time)) %>%
