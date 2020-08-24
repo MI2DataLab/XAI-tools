0caa0696aac753def4903aa69958ff3de50f4e3f
jusira <alicjagosiewska@gmail.com>
2020-08-24 12:34:02 +0200

    re-run rmd files and add them to docs folder

--- benchmark/generate_results.R
+++ benchmark/generate_results.R
@@ -1,14 +1,24 @@
 library(xtable)
 library(dplyr)
+library(tidyr)
 
 load("./benchmark/packages_times.rda")
 
-packages_times %>%
+summary_time <- packages_times %>%
   select(pkg_name, evaluation_time) %>%
   group_by(pkg_name) %>%
-  mutate(min = min(evaluation_time),
-         mean = mean(evaluation_time),
-         max = max(evaluation_time)) %>%
-  select(-evaluation_time) %>%
+  add_count(pkg_name) %>%
+  mutate(sum = round(sum(evaluation_time),2)) %>%
+  select(pkg_name, n, sum) %>%
+  unique()
+
+
+packages_times %>%
+  select(pkg_name, type, evaluation_time) %>%
+  group_by(pkg_name, type) %>%
+  mutate(evaluation_time = round(median(evaluation_time), 2)) %>%
   unique() %>%
+  pivot_wider(names_from = type, values_from = evaluation_time)  %>%
+  mutate_all(~replace(., is.na(.), "-")) %>%
+  bind_cols(summary_time[, c("sum", "n")]) %>%
   xtable()
